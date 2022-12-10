b6018a422a363bb179ac0e47a51d8827f6c5b2a4
adhasanblog <fhasanblog@gmail.com>
2022-12-10 15:12:32 +0700

    movie catalogue with websocket

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,8 @@ import '../styles/responsive.css';
 import App from './views/app';
 import './utils/fontawesome-love';
 import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocker-initiator';
+import CONFIG from './globals/config';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -18,4 +20,6 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
+
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,4 +1,5 @@
 import Detail from '../views/pages/detail';
+import Like from '../views/pages/like';
 import NowPlaying from '../views/pages/now-playing';
 import Upcoming from '../views/pages/upcoming';
 
@@ -6,6 +7,7 @@ const routes = {
   '/': NowPlaying, //Default Page
   '/now-playing': NowPlaying,
   '/upcoming': Upcoming,
+  '/like': Like,
   '/detail/:id': Detail,
 };
 
